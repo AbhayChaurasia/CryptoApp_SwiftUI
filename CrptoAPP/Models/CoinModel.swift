5cd3ac952eb3d233ce9e403e60b88e3bfc1b6efe
Abhay Chaurasia <ac37636@gmail.com>
2025-01-19 21:40:01 +0530

    third commit

--- CrptoAPP/Models/CoinModel.swift
+++ CrptoAPP/Models/CoinModel.swift
@@ -11,12 +11,13 @@ import Foundation
 
 
 
-struct Model {
+struct CoinModel : Identifiable , Codable  {
     
     var id, symbol, name: String?
     var image: String?
-    var currentPrice, marketCap, marketCapRank, fullyDilutedValuation: Int?
-    var totalVolume, high24H, low24H: Int?
+    var currentPrice: Double?
+    var  marketCap, marketCapRank, fullyDilutedValuation: Int?
+    var totalVolume, high24H, low24H: Double?
     var priceChange24H, priceChangePercentage24H: Double?
     var marketCapChange24H: Int?
     var marketCapChangePercentage24H: Double?
@@ -27,13 +28,27 @@ struct Model {
     var atlDate: String?
     var roi: String?
     var lastUpdated: String?
-    var sparklineIn7D: String?
+    var sparklineIn7D: SparklineIn7D?
     var priceChangePercentage24HInCurrency: Double?
+    var priceChangePercentage24HInCurrency_Second: Double?
+    let currentHolding : Double?
     
+    enum CodingKeys : String , CodingKey{
+     
+        
+        case id, symbol, name  : String
+    }
+    
+//    enum CodingKeys: String, CodingKey {
+    //       }
+//           case id, symbol, name  : String
+//           case image : String
+//           case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
+
 }
 
 
-struct SparklineIn7D  
+struct SparklineIn7D   : Codable
 {
     var price: [Double]?
 }
